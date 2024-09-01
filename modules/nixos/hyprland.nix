400a5c2f748bad163e4ce607927081183a37e8e2
Corwin Perren <caperren@gmail.com>
2024-08-31 20:35:24 -0700

    Nvidia, audio, and esp32 work

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -20,6 +20,8 @@
     WLR_NO_HARDWARE_CURSORS = "1";
     # Hint electron apps to use wayland
     NIXOS_OZONE_WL = "1";
+    # Fix waiting on vsync
+    __GL_SYNC_TO_VBLANK = "0";
   };
 
   services.xserver = {
@@ -45,7 +47,8 @@
     # Enable modesetting for Wayland compositors (hyprland)
     modesetting.enable = true;
     # Use the open source version of the kernel module (for driver 515.43.04+)
-    # open = true;
+    # Actually, just overridden to false for now
+    open = false;
     # Enable the Nvidia settings menu
     nvidiaSettings = true;
     # Select the appropriate driver version for your specific GPU

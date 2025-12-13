b110daed58e40097ead5803bbba1349c66d5db6f
Corwin Perren <caperren@gmail.com>
2025-12-13 15:09:38 -0800

    Re-enable primary server

--- modules/application-groups/k3s-primary.nix
+++ modules/application-groups/k3s-primary.nix
@@ -3,7 +3,7 @@
   sops.secrets.k3s_token.sopsFile = ../../secrets/cluster.yaml;
 
   services.k3s = {
-    enable = false;
+    enable = true;
     role = "server";
     tokenFile = config.sops.secrets.k3s_token.path;
     clusterInit = true;

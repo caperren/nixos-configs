d72c3d4e567eb56a7f49a5d27a024aaa02699d19
Corwin Perren <caperren@gmail.com>
2025-12-13 15:28:48 -0800

    Re-enable secondaries

--- modules/application-groups/k3s-secondary.nix
+++ modules/application-groups/k3s-secondary.nix
@@ -3,7 +3,7 @@
   sops.secrets.k3s_token.sopsFile = ../../secrets/cluster.yaml;
 
   services.k3s = {
-    enable = false;
+    enable = true;
     role = "server"; # Or "agent" for worker only nodes
     tokenFile = config.sops.secrets.k3s_token.path;
     serverAddr = "https://cap-clust-01:6443";

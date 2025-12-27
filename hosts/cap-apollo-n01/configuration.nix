5c327a7cedc0d23b531cd576dcec69adf89e4f0d
Corwin Perren <caperren@gmail.com>
2025-12-26 23:31:49 -0800

    Fix import paths

--- hosts/cap-apollo-n01/configuration.nix
+++ hosts/cap-apollo-n01/configuration.nix
@@ -8,17 +8,17 @@
     ../../users/caperren/caperren.nix
 
     # System Configuration
-    ../system/cpu-intel.nix
-    ../system/fonts.nix
-    ../system/home-manager-settings.nix
-    ../system/internationalization.nix
-    ../system/networking.nix
-    ../system/nix-settings.nix
-    ../system/security.nix
-    ../system/systemd-boot.nix
+    ../../system/cpu-intel.nix
+    ../../system/fonts.nix
+    ../../system/home-manager-settings.nix
+    ../../system/internationalization.nix
+    ../../system/networking.nix
+    ../../system/nix-settings.nix
+    ../../system/security.nix
+    ../../system/systemd-boot.nix
 
     # Application Groups
-    ../application-groups/system-utilities-cluster.nix
+    ../../application-groups/system-utilities-cluster.nix
   ];
 
   networking.hostName = "cap-apollo-n01";

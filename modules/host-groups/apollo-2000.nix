2800090de6bfe13908d95fcefc9b4898ad12cc0a
Corwin Perren <caperren@gmail.com>
2026-01-01 19:07:02 -0800

    Check if import order matters

--- modules/host-groups/apollo-2000.nix
+++ modules/host-groups/apollo-2000.nix
@@ -23,11 +23,11 @@
     ../application-groups/virtualization.nix
 
     # Kubernetes Applications
+    ../kubernetes/apollo-2000/secrets.nix
     ../kubernetes/apollo-2000/grafana.nix
     ../kubernetes/apollo-2000/helm-hello-world.nix
     ../kubernetes/apollo-2000/node-exporter.nix
     ../kubernetes/apollo-2000/prometheus.nix
-    ../kubernetes/apollo-2000/secrets.nix
   ];
 
   time.timeZone = "America/Los_Angeles";

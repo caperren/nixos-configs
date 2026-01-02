398907f8bb3dad496dcce52c0a86117d7c35e186
Corwin Perren <caperren@gmail.com>
2026-01-01 18:54:19 -0800

    Reference path in secrets rather than dot notation

--- modules/kubernetes/apollo-2000/secrets.nix
+++ modules/kubernetes/apollo-2000/secrets.nix
@@ -1,13 +1,13 @@
 { config, ... }:
 {
   sops = {
-    secrets.grafana.admin-password.sopsFile = ../../../secrets/apollo-2000.yaml;
+    secrets."grafana/admin-password".sopsFile = ../../../secrets/apollo-2000.yaml;
     templates.grafanaAdmin = {
       content = builtins.toJSON {
         apiVersion = "v1";
         kind = "Secret";
         metadata.name = "grafana-admin";
-        stringData.password = config.sops.grafana.admin-password;
+        stringData.password = config.sops."grafana/admin-password";
       };
       path = "/var/lib/rancher/k3s/server/manifests/grafana-admin-secret.json";
     };

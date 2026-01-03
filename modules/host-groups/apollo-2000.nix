84923cc44f9bf1c5ca1d53c10520c05b3967b56b
Corwin Perren <caperren@gmail.com>
2026-01-02 19:50:53 -0800

    Disable immich

--- modules/host-groups/apollo-2000.nix
+++ modules/host-groups/apollo-2000.nix
@@ -30,7 +30,7 @@
     ../kubernetes/apollo-2000/grafana.nix
 #    ../kubernetes/apollo-2000/helm-hello-world.nix
     ../kubernetes/apollo-2000/home-assistant.nix
-    ../kubernetes/apollo-2000/immich.nix
+#    ../kubernetes/apollo-2000/immich.nix
     ../kubernetes/apollo-2000/node-exporter.nix
     ../kubernetes/apollo-2000/prometheus.nix
   ];

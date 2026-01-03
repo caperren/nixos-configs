822f12fd5d652dfe97c11b55b60d927685536e6b
Corwin Perren <caperren@gmail.com>
2026-01-02 21:07:21 -0800

    Added spliit

--- modules/host-groups/apollo-2000.nix
+++ modules/host-groups/apollo-2000.nix
@@ -34,6 +34,7 @@
     ../kubernetes/apollo-2000/node-exporter.nix
     ../kubernetes/apollo-2000/plex.nix
     ../kubernetes/apollo-2000/prometheus.nix
+    ../kubernetes/apollo-2000/spliit.nix
     ../kubernetes/apollo-2000/technitium.nix
   ];
 

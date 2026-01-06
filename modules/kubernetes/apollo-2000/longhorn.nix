897f42042e929578e491a12971fb776a521df3b8
Corwin Perren <caperren@gmail.com>
2026-01-06 13:44:36 -0800

    Re-nable longhorn with new mount

--- modules/kubernetes/apollo-2000/longhorn.nix
+++ modules/kubernetes/apollo-2000/longhorn.nix
@@ -56,7 +56,7 @@ in
             defaultSettings:
               defaultReplicaCount: ${toString defaultReplicaCount}
               # Where Longhorn stores data on each node:
-              defaultDataPath: /kubernetes_data
+              defaultDataPath: /mnt/longhorn
 
               # Make sure we don't overuse the data mount
               storageOverProvisioningPercentage: 100

0fb9230fcfee2612d31403771adf5fae99fdf5cd
Corwin Perren <caperren@gmail.com>
2026-01-05 22:45:03 -0800

    Make full longhorn path

--- modules/host-groups/apollo-2000-k3s-cluster.nix
+++ modules/host-groups/apollo-2000-k3s-cluster.nix
@@ -98,7 +98,7 @@
                 sleep 1;
             done
             mkfs.ext4 /dev/zvol/kubernetes_data/longhorn-ext4
-            if [ ! -d "/mnt" ]; then mkdir -p /mnt; fi
+            if [ ! -d "/mnt/longhorn" ]; then mkdir -p /mnt/longhorn; fi
           fi
         ''}";
 

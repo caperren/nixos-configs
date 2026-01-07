83d83897d5567afe51c3438d16aa8726017fabd3
Corwin Perren <caperren@gmail.com>
2026-01-06 21:35:38 -0800

    Add wireguard client config for apollo n02

--- hosts/cap-apollo-n02/configuration.nix
+++ hosts/cap-apollo-n02/configuration.nix
@@ -10,4 +10,24 @@
 
   networking.hostName = "cap-apollo-n02";
   networking.hostId = "bc7334b5";
+
+  sops.secrets."cap-apollo-n02/wireguard/private-key".sopsFile = ../../secrets/apollo-2000.yaml;
+
+  # Wireguard connection to my vps, for tunnelled reverse-proxying
+  networking.wireguard.interfaces = {
+    wg0 = {
+      ips = [ "10.8.0.4/24" ];
+      listenPort = 51820;
+      privateKeyFile = config.sops.secrets."cap-apollo-n02/wireguard/private-key".path;
+
+      peers = [
+        {
+          publicKey = "EiFCVUvibomC8du68TGYvWYi/haNv0MELPJvnhPAcHA=";
+          allowedIPs = [ "0.0.0.0/0" ];
+          endpoint = "caperren.com:51820";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
 }

9c2e78382a690bafe1cbf208ec30d0689bed0271
Corwin Perren <caperren@gmail.com>
2026-01-07 16:26:54 -0800

    Explicit k3s primaries array

--- modules/application-groups/k3s.nix
+++ modules/application-groups/k3s.nix
@@ -13,6 +13,10 @@ let
     "cap-clust-02" = ../../secrets/cluster.yaml;
     "cap-clust-03" = ../../secrets/cluster.yaml;
   };
+  k3sPrimaries = [
+    "cap-apollo-n02"
+    "cap-clust-n01"
+  ];
   k3sNodeToPrimary = {
     "cap-apollo-n03" = "https://cap-apollo-n02:6443";
     "cap-apollo-n04" = "https://cap-apollo-n02:6443";
@@ -20,9 +24,7 @@ let
     "cap-clust-03" = "https://cap-clust-01:6443";
   };
 
-  isK3sPrimary = lib.lists.elem "${config.networking.hostName}" (
-    lib.unique (lib.attrValues k3sNodeToPrimary)
-  );
+  isK3sPrimary = lib.lists.elem "${config.networking.hostName}" k3sPrimaries;
   serverAddr = if isK3sPrimary then "" else k3sNodeToPrimary.${config.networking.hostName};
 in
 {

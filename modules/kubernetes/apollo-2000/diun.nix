a02595f609983d38c5910435938e9ef61d4dd56f
Corwin Perren <caperren@gmail.com>
2026-01-09 20:59:14 -0800

    Try other code alias

--- modules/kubernetes/apollo-2000/diun.nix
+++ modules/kubernetes/apollo-2000/diun.nix
@@ -140,7 +140,7 @@ lib.mkIf (config.networking.hostName == "cap-apollo-n02") {
                     }
                     {
                       name = " DIUN_NOTIF_TELEGRAM_TEMPLATEBODY";
-                      value = "Found updated image {{ .Entry.Image.Path }}.%0A`nix-shell -p nix-prefetch-docker --run \"nix-prefetch-docker --image-name {{ .Entry.Image }} --image-tag {{ .Entry.Image.Tag }}\"`";
+                      value = "Found updated image {{ .Entry.Image.Path }}.%0A<code>nix-shell -p nix-prefetch-docker --run \"nix-prefetch-docker --image-name {{ .Entry.Image }} --image-tag {{ .Entry.Image.Tag }}\"</code>";
                     }
                     {
                       name = "DIUN_WATCH_FIRSTCHECKNOTIF";

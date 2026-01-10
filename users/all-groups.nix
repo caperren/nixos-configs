97ec759968442dbc9a863b9ce741b70671e5f346
Corwin Perren <caperren@gmail.com>
2026-01-10 00:27:29 -0800

    Put groups back the way they were

--- users/all-groups.nix
+++ users/all-groups.nix
@@ -4,35 +4,18 @@
   # gid=1(wheel)
   # gid=100(users)
 
-  users.groups = {
-    # Group with r/w access to all nas shares
-    nas-caperren = {
-      gid = 200;
-      isSystemGroup = true;
-    };
+  # Group with r/w access to all nas shares
+  users.groups.nas-caperren.gid = 200;
 
-    # Group with r/w permissions to the media share
-    nas-media-management = {
-      gid = 201;
-      isSystemGroup = true;
-    };
+  # Group with r/w permissions to the media share
+  users.groups.nas-media-management.gid = 201;
 
-    # Group with r permissions to the media share
-    nas-media-view = {
-      gid = 202;
-      isSystemGroup = true;
-    };
+  # Group with r permissions to the media share
+  users.groups.nas-media-view.gid = 202;
 
-    # Group with r/w permissions to the ad share
-    nas-ad-management = {
-      gid = 203;
-      isSystemGroup = true;
-    };
+  # Group with r/w permissions to the ad share
+  users.groups.nas-ad-management.gid = 203;
 
-    # Group with r permissions to the ad share
-    nas-ad-view = {
-      gid = 204;
-      isSystemGroup = true;
-    };
-  };
+  # Group with r permissions to the ad share
+  users.groups.nas-ad-view.gid = 204;
 }

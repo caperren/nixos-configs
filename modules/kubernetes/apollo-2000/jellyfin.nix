705f06e5d33980ff43b845f63f52f6dc80a4f6b4
Corwin Perren <caperren@gmail.com>
2026-01-10 21:03:49 -0800

    Scale jellyfin to 0 to redo pvc

--- modules/kubernetes/apollo-2000/jellyfin.nix
+++ modules/kubernetes/apollo-2000/jellyfin.nix
@@ -25,7 +25,7 @@ lib.mkIf (config.networking.hostName == "cap-apollo-n02") {
           labels."app.kubernetes.io/name" = "jellyfin";
         };
         spec = {
-          replicas = 1;
+          replicas = 0;
           strategy = {
             type = "RollingUpdate";
             rollingUpdate = {

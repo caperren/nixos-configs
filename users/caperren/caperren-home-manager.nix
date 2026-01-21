7064a141149f78f3a4aa6bb6d22affd5eb9569a3
Corwin Perren <caperren@gmail.com>
2026-01-20 18:57:46 -0800

    Use nix-index for command not found, so I know what package to add when it's missing

--- users/caperren/caperren-home-manager.nix
+++ users/caperren/caperren-home-manager.nix
@@ -54,7 +54,13 @@ in
       };
 
     };
+
     programs.bash.enable = true;
+    programs.command-not-found.enable = false;
+    programs.bash.interactiveShellInit = ''
+      source ${pkgs.nix-index}/etc/profile.d/command-not-found.sh
+    '';
+
     programs.bemenu.enable = true;
 
     programs.kitty = {

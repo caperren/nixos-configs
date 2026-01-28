506acb2801af5a8b011ac4b173a6675bf9d38fca
Corwin Perren <caperren@gmail.com>
2026-01-27 19:24:09 -0800

    Add stash b, fix ingress

--- modules/kubernetes/apollo-2000/stash.nix
+++ modules/kubernetes/apollo-2000/stash.nix
@@ -44,7 +44,7 @@ in
   services.k3s = lib.mkIf (config.networking.hostName == "cap-apollo-n02") {
     images = [ image ];
     manifests = {
-      # Shared
+      # A
       stash-a-deployment.content = {
         apiVersion = "apps/v1";
         kind = "Deployment";
@@ -204,15 +204,15 @@ in
           ];
         };
       };
-      jellyfin-service.content = {
+      stash-a-service.content = {
         apiVersion = "v1";
         kind = "Service";
         metadata = {
-          name = "jellyfin";
-          labels."app.kubernetes.io/name" = "jellyfin";
+          name = "stash-a";
+          labels."app.kubernetes.io/name" = "stash-a";
         };
         spec = {
-          selector."app.kubernetes.io/name" = "jellyfin";
+          selector."app.kubernetes.io/name" = "stash-a";
           ports = [
             {
               port = 9999;
@@ -259,7 +259,220 @@ in
           ];
         };
 
-        # Local
+        # B
+        stash-b-deployment.content = {
+        apiVersion = "apps/v1";
+        kind = "Deployment";
+        metadata = {
+          name = "stash-b";
+          labels."app.kubernetes.io/name" = "stash-b";
+        };
+        spec = {
+          replicas = allowedReplicas;
+          strategy = {
+            type = "RollingUpdate";
+            rollingUpdate = {
+              maxSurge = 0;
+              maxUnavailable = 1;
+            };
+          };
+
+          selector.matchLabels."app.kubernetes.io/name" = "stash-b";
+
+          template = {
+           metadata = {
+              labels."app.kubernetes.io/name" = "stash-b";
+              annotations."diun.enable" = "true";
+            };
+            spec = {
+              securityContext.supplementalGroups = [ config.users.groups.nas-ad-view.gid ];
+              containers = [
+                {
+                  name = "stash-b";
+                  image = "${image.imageName}:${image.imageTag}";
+                  imagePullPolicy = "IfNotPresent";
+                  env = stashShareEnvironment;
+                  ports = [ { containerPort = 9999; } ];
+                  volumeMounts = [
+                    {
+                      mountPath = "/store";
+                      name = "store";
+                    }
+                    {
+                      mountPath = "/root/.stash";
+                      name = "config";
+                    }
+                    {
+                      mountPath = "/data";
+                      name = "content";
+                    }
+                    {
+                      mountPath = "/cache";
+                      name = "cache";
+                    }
+                  ];
+                }
+              ];
+              volumes = [
+                {
+                  name = "cache";
+                  emptyDir = { };
+                }
+                {
+                  name = "config";
+                  persistentVolumeClaim.claimName = "stash-b-config-pvc";
+                }
+                {
+                  name = "store";
+                  persistentVolumeClaim.claimName = "stash-b-store-pvc";
+                }
+                {
+                  name = "content";
+                  persistentVolumeClaim.claimName = "stash-b-content-pvc";
+                }
+              ];
+            };
+          };
+        };
+      };
+      stash-b-config-pvc.content = {
+        apiVersion = "v1";
+        kind = "PersistentVolumeClaim";
+        metadata = {
+          name = "stash-b-config-pvc";
+          labels."app.kubernetes.io/name" = "stash-b";
+        };
+        spec = {
+          accessModes = [ "ReadWriteOnce" ];
+          storageClassName = "longhorn";
+          resources.requests.storage = "256Mi";
+        };
+      };
+      stash-b-store-pvc.content = {
+        apiVersion = "v1";
+        kind = "PersistentVolumeClaim";
+        metadata = {
+          name = "stash-b-store-pvc";
+          labels."app.kubernetes.io/name" = "stash-b";
+        };
+        spec = {
+          accessModes = [ "ReadWriteOnce" ];
+          storageClassName = "longhorn";
+          resources.requests.storage = "2Gi";
+        };
+      };
+      stash-b-content-nfs-pv.content = {
+        apiVersion = "v1";
+        kind = "PersistentVolume";
+        metadata = {
+          name = "stash-b-content-nfs-pv";
+          labels."app.kubernetes.io/name" = "stash-b";
+        };
+        spec = {
+          capacity.storage = "1Ti";
+          accessModes = [ "ReadOnlyMany" ];
+          persistentVolumeReclaimPolicy = "Retain";
+          mountOptions = [
+            "nfsvers=4.1"
+            "rsize=1048576"
+            "wsize=1048576"
+            "hard"
+            "timeo=600"
+            "retrans=2"
+          ];
+          nfs = {
+            server = "cap-apollo-n01";
+            path = "/nas_data_primary/ad";
+            readOnly = true;
+          };
+        };
+      };
+      stash-b-content-pvc.content = {
+        apiVersion = "v1";
+        kind = "PersistentVolumeClaim";
+        metadata = {
+          name = "stash-b-content-pvc";
+          labels."app.kubernetes.io/name" = "stash-b";
+        };
+        spec = {
+          selector.matchLabels."app.kubernetes.io/name" = "stash-b";
+          accessModes = [ "ReadOnlyMany" ];
+          volumeName = "stash-b-content-nfs-pv";
+          storageClassName = "";
+          resources.requests.storage = "1Ti";
+        };
+      };
+      stash-b-service.content = {
+        apiVersion = "v1";
+        kind = "Service";
+        metadata = {
+          name = "stash-b";
+          labels."app.kubernetes.io/name" = "stash-b";
+        };
+        spec = {
+          selector."app.kubernetes.io/name" = "stash-b";
+          ports = [
+            {
+              port = 9999;
+              targetPort = 9999;
+            }
+          ];
+        };
+      };
+      stash-b-service.content = {
+        apiVersion = "v1";
+        kind = "Service";
+        metadata = {
+          name = "stash-b";
+          labels."app.kubernetes.io/name" = "stash-b";
+        };
+        spec = {
+          selector."app.kubernetes.io/name" = "stash-b";
+          ports = [
+            {
+              port = 9999;
+              targetPort = 9999;
+            }
+          ];
+        };
+      };
+      stash-b-ingress.content = {
+        apiVersion = "networking.k8s.io/v1";
+        kind = "Ingress";
+        metadata = {
+          name = "stash-b";
+          labels."app.kubernetes.io/name" = "stash-b";
+          annotations = {
+            "traefik.ingress.kubernetes.io/router.entrypoints" = "web";
+            "gethomepage.dev/description" = "Ad content serving";
+            "gethomepage.dev/enabled" = "true";
+            "gethomepage.dev/group" = "Media";
+            "gethomepage.dev/icon" = "stash.png";
+            "gethomepage.dev/name" = "stash-b";
+          };
+        };
+        spec = {
+          ingressClassName = "traefik";
+          rules = [
+            {
+              host = "stash-b.internal.perren.cloud";
+              http = {
+                paths = [
+                  {
+                    path = "/";
+                    pathType = "Prefix";
+                    backend = {
+                      service = {
+                        name = "stash-b";
+                        port.number = 9999;
+                      };
+                    };
+                  }
+                ];
+              };
+            }
+          ];
+        };
       };
     };
   };

294c1d3cab2408060b4cf2d44e2402f47699cadf
Corwin Perren <caperren@gmail.com>
2026-02-13 14:21:00 -0800

    Don't upgrade longhorn quite yet

--- modules/kubernetes/apollo-2000/longhorn.nix
+++ modules/kubernetes/apollo-2000/longhorn.nix
@@ -90,7 +90,7 @@ in
           chart = "longhorn";
           targetNamespace = "longhorn-system";
 
-          version = "v1.11.0";
+          version = "v1.10.1";
 
           valuesContent = ''
             # Make Longhorn create/mark its StorageClass as the default

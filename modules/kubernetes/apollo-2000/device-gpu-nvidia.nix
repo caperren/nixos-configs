df28930ff23dd44d43615a694ba1765a2894f443
Corwin Perren <caperren@gmail.com>
2026-02-23 14:37:20 -0800

    Enable cdi plugin for gpu passthrough

--- modules/kubernetes/apollo-2000/device-gpu-nvidia.nix
+++ modules/kubernetes/apollo-2000/device-gpu-nvidia.nix
@@ -8,6 +8,16 @@ let
   gpuHavingNodeName = "cap-apollo-n04";
 in
 {
+  hardware.nvidia-container-toolkit.enable = true;
+  virtualisation.containerd = {
+    enable = true;
+    settings = {
+      plugins."io.containerd.grpc.v1.cri" = {
+        enable_cdi = true;
+        cdi_spec_dirs = [ "/var/run/cdi" ];
+      };
+    };
+  };
   services.k3s = {
     containerdConfigTemplate = ''
       {{ template "base" . }}
@@ -21,14 +31,112 @@ in
       [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.nvidia.options]
         BinaryName = "${pkgs.nvidia-container-toolkit.tools}/bin/nvidia-container-runtime.cdi"
     '';
+    extraFlags = (
+      toString [
+        "--container-runtime-endpoint unix:///run/containerd/containerd.sock"
+      ]
+    );
+    label."nixos-nvidia-cdi" = "enabled";
+
     manifests = lib.mkIf (config.networking.hostName == "cap-apollo-n02") {
+      nvidia-gpu-node-labeler.content = {
+        apiVersion = "v1";
+        kind = "Node";
+        metadata = {
+          name = "nvidia";
+          labels."nixos-nvidia-cdi" = "gpu-enabled";
+
+        };
+      };
       nvidia-runtime-class.content = {
         apiVersion = "node.k8s.io/v1";
         kind = "RuntimeClass";
         handler = "nvidia";
         metadata = {
-          labels."app.kubernetes.io/component" = "gpu-operator";
           name = "nvidia";
+          labels."app.kubernetes.io/component" = "gpu-operator";
+        };
+      };
+      generic-cdi-namespace.content = {
+        apiVersion = "v1";
+        kind = "Namespace";
+        metadata.name = "generic-cdi-plugin";
+      };
+      generic-cdi-plugin-daemonset.content = {
+        apiVersion = "apps/v1";
+        kind = "DaemonSet";
+        metadata = {
+          name = "generic-cdi-plugin-daemonset";
+          namespace = "generic-cdi-plugin";
+        };
+        spec = {
+          selector.matchLabels.name = "generic-cdi-plugin";
+          template = {
+            metadata.labels = {
+              name = "generic-cdi-plugin";
+              "app.kubernetes.io/component" = "generic-cdi-plugin";
+              "app.kubernetes.io/name" = "generic-cdi-plugin";
+            };
+          };
+
+          spec = {
+            containers = [
+              {
+                image = "ghcr.io/olfillasodikno/generic-cdi-plugin:main";
+                name = "generic-cdi-plugin";
+                command = [
+                  "/generic-cdi-plugin"
+                  "/var/run/cdi/nvidia-container-toolkit.json"
+                ];
+                imagePullPolicy = "Always";
+                securityContext.privileged = true;
+                tty = true;
+                volumeMounts = [
+                  {
+                    name = "kubelet";
+                    mountPath = "/var/lib/kubelet";
+                  }
+                  {
+                    name = "nvidia-container-toolkit";
+                    mountPath = "/var/run/cdi/nvidia-container-toolkit.json";
+                  }
+                ];
+              }
+            ];
+
+            volumes = [
+              {
+                name = "kubelet";
+                hostPath = {
+                  path = "/var/lib/kubelet";
+                };
+              }
+              {
+                name = "nvidia-container-toolkit";
+                hostPath = {
+                  path = "/var/run/cdi/nvidia-container-toolkit.json";
+                };
+              }
+            ];
+
+            affinity = {
+              nodeAffinity = {
+                requiredDuringSchedulingIgnoredDuringExecution = {
+                  nodeSelectorTerms = [
+                    {
+                      matchExpressions = [
+                        {
+                          key = "nixos-nvidia-cdi";
+                          operator = "In";
+                          values = [ "enabled" ];
+                        }
+                      ];
+                    }
+                  ];
+                };
+              };
+            };
+          };
         };
       };
     };

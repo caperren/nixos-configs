391823b68eda745f6662f416f80b58c51d3bffb6
Corwin Perren <caperren@gmail.com>
2026-02-23 10:19:08 -0800

    Explicit flannel config

--- modules/application-groups/k3s.nix
+++ modules/application-groups/k3s.nix
@@ -26,6 +26,13 @@ let
 
   isK3sPrimary = lib.lists.elem config.networking.hostName k3sPrimaries;
   serverAddr = if isK3sPrimary then "" else k3sNodeToPrimary.${config.networking.hostName};
+
+  flannelSubnetEnv = pkgs.writeText "subnet.env" ''
+    FLANNEL_NETWORK=10.42.0.0/16
+    FLANNEL_SUBNET=10.42.1.1/24
+    FLANNEL_MTU=1450
+    FLANNEL_IPMASQ=true
+  '';
 in
 {
   sops.secrets.k3s_token.sopsFile = k3sTokenSopsFile.${config.networking.hostName};
@@ -37,6 +44,10 @@ in
         "*":
     '';
   };
+
+  systemd.tmpfiles.rules = [
+    "L+ /run/flannel/subnet.env - - - - ${flannelSubnetEnv}"
+  ];
 
   services.k3s = {
     enable = true;

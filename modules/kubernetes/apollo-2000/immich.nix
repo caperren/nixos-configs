74c978a9349276a6cbc22805ca889e409d9d3557
Corwin Perren <caperren@gmail.com>
2026-02-24 22:33:20 -0800

    Increase jellyfin pvc size, immich images

--- modules/kubernetes/apollo-2000/immich.nix
+++ modules/kubernetes/apollo-2000/immich.nix
@@ -5,17 +5,24 @@
   ...
 }:
 let
-  image = pkgs.dockerTools.pullImage {
+  immichServerImage = pkgs.dockerTools.pullImage {
     imageName = "ghcr.io/immich-app/immich-server";
-    imageDigest = "sha256:e6a6298e67ae077808fdb7d8d5565955f60b0708191576143fc02d30ab1389d1";
-    hash = "sha256-iB92wUlMfsWuWjuv87Gc6HbWl1PzZl89jgoCveix1JA=";
-    finalImageTag = "v2.4.1";
-    arch = "amd64";
+    imageDigest = "sha256:aa163d2e1cc2b16a9515dd1fef901e6f5231befad7024f093d7be1f2da14341a";
+    hash = "sha256-VRqUD6mVub5qoIL6zt5iy4jk7rBm6Y4ddU/+o724q2g=";
+    finalImageName = "ghcr.io/immich-app/immich-server";
+    finalImageTag = "v2.5.6";
+  };
+  immichPostgresImage = pkgs.dockerTools.pullImage {
+    imageName = "ghcr.io/immich-app/postgres";
+    imageDigest = "sha256:bcf63357191b76a916ae5eb93464d65c07511da41e3bf7a8416db519b40b1c23";
+    hash = "sha256-YNxadMZRLd2Wky6UvZFTcJ+KXFxg51E4zPdEAG7HtPk=";
+    finalImageName = "ghcr.io/immich-app/postgres";
+    finalImageTag = "14-vectorchord0.4.3-pgvectors0.2.0";
   };
 in
 lib.mkIf (config.networking.hostName == "cap-apollo-n02") {
   services.k3s = {
-    images = [ image ];
+    images = [ immichServerImage immichPostgresImage ];
     manifests = {
       immich-deployment.content = {
         apiVersion = "apps/v1";

d4bb59cd92edfbc747d3ba9d3a530d16d8b1412b
Corwin Perren <caperren@gmail.com>
2026-03-01 22:16:56 -0800

    New wireguard server testing

--- hosts/cap-hetz-01/configuration.nix
+++ hosts/cap-hetz-01/configuration.nix
@@ -50,6 +50,13 @@ in
   networking = {
     hostName = "cap-hetz-01";
 
+    nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
+    ];
+
     enableIPv6 = true;
 
     wireguard = {

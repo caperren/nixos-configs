54bb6df97902e67d088ac093350c7f60e5d7cd99
Corwin Perren <caperren@gmail.com>
2026-06-25 20:07:43 -0700

    ai model tweaks

--- hosts/cap-apollo-n04/configuration.nix
+++ hosts/cap-apollo-n04/configuration.nix
@@ -26,18 +26,28 @@
     environmentVariables = {
       CUDA_VISIBLE_DEVICES = "0"; # or "1" for the other NVIDIA GPU
     };
+    #    loadModels = [
+    #      #      "llama3.2:3b"
+    #      #      "phi4-reasoning:14b"
+    #      #      "dolphin3:8b"
+    #      #      "smallthinker:3b"
+    #      #      "gemma3:4b"
+    #      "gemma4:26b"
+    #      #      "gemma3:27b"
+    #      "deepcoder:14b"
+    #      "qwen3.6:27b"
+    #      #      "qwen3.6:27b"
+    #      #      "nomic-embed-text"
+    #    ];
     loadModels = [
-      #      "llama3.2:3b"
-      #      "phi4-reasoning:14b"
-      #      "dolphin3:8b"
-      #      "smallthinker:3b"
-      #      "gemma3:4b"
-      "gemma4:26b"
-      #      "gemma3:27b"
-      "deepcoder:14b"
-      "qwen3.6:27b"
-      #      "qwen3.6:27b"
-      #      "nomic-embed-text"
+      "qwen3:8b"
+      "qwen3:14b"
+      "qwen2.5-coder:1.5b"
+      "qwen2.5-coder:7b"
+      "qwen2.5-coder:14b"
+      "devstral-small-2"
+      "qwen3-embedding:4b"
+      "qwen3-vl:8b"
     ];
   };
 

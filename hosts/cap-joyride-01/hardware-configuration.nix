fd11123896b5b1763bfd92430285b428179c483b
Corwin Perren <caperren@gmail.com>
2026-08-28 10:23:45 -0700

    USB key for booting joyride

--- hosts/cap-joyride-01/hardware-configuration.nix
+++ hosts/cap-joyride-01/hardware-configuration.nix
@@ -31,8 +31,13 @@
     fsType = "ext4";
   };
 
-  boot.initrd.luks.devices."luks-e24d5b44-c3a8-4f35-8850-aa11c901eab9".device =
-    "/dev/disk/by-uuid/e24d5b44-c3a8-4f35-8850-aa11c901eab9";
+  boot.initrd.luks.devices."luks-e24d5b44-c3a8-4f35-8850-aa11c901eab9" = {
+    device = "/dev/disk/by-uuid/e24d5b44-c3a8-4f35-8850-aa11c901eab9";
+    allowDiscards = true;
+    keyFileSize = 4096;
+    keyFile = "usb-_Patriot_Memory_07B406013A4D761C-0:0";
+    fallbackToPassword = true;
+  };
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/0249-5787";

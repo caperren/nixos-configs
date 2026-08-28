64c9963635ec2699007d324b5df0600a49846a4e
Corwin Perren <caperren@gmail.com>
2026-08-27 21:38:48 -0700

    Syncthing tweaking

--- constants/syncthing.nix
+++ constants/syncthing.nix
@@ -14,6 +14,11 @@ let
       id = "YSL2OXD-62M5Z6G-ID5LDD5-7MGHMTQ-3QTEXB4-NHOZIHH-5KX4F4B-6RIL5A4";
     };
   };
+  cap_joyride_01 = {
+    "cap-joyride-01" = {
+      id = "6YG34W5-52EXEAS-4RTGLCM-JOSGICK-M6QIRQS-OLLRWTF-HDZMNRP-ZJ24FAM";
+    };
+  };
   android = {
     "android" = {
       id = "GTAF4KD-BTLBC7H-X2HRHLR-UFBU5B4-LE4TSYP-F2VKVV7-JASWFJQ-CT4B5AF";
@@ -22,6 +27,6 @@ let
 in
 
 {
-  allDevices = cap_apollo_n01 // cap_nr200p // cap_slim7 // android;
-  nonAndroidDevices = cap_apollo_n01 // cap_nr200p // cap_slim7;
+  allDevices = cap_apollo_n01 // cap_nr200p // cap_slim7 // cap_joyride_01 // android;
+  nonAndroidDevices = cap_apollo_n01 // cap_nr200p // cap_slim7 // cap_joyride_01;
 }
